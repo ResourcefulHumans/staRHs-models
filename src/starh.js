5337c4df8f307b8c7bdcf8b50fd7b712b1959c79
Markus Tacker <m@coderbyheart.com>
2016-12-15 18:07:34 +0100

    feat(StaRH): from/to has name and avatar

--- src/starh.js
+++ src/starh.js
@@ -1,4 +1,4 @@
-import {String as StringType, Number as NumberType, irreducible, refinement} from 'tcomb'
+import {String as StringType, Number as NumberType, irreducible, refinement, struct} from 'tcomb'
 import URIValue from 'rheactor-value-objects/uri'
 import {Model} from './model'
 import {merge} from 'lodash'
@@ -7,13 +7,13 @@ const PositiveIntegerType = refinement(NumberType, n => n > 0 && n % 1 === 0, 'P
 
 export class StaRH extends Model {
   /**
-   * @param {{from: string, to: string, amount: number, message: string}} fields
+   * @param {{from: PersonType, to: PersonType, amount: number, message: string}} fields
    */
   constructor (fields) {
     super($context)
     const {from, to, amount, message} = fields
-    StringType(from)
-    StringType(to)
+    PersonType(from)
+    PersonType(to)
     PositiveIntegerType(amount)
     StringType(message)
     this.from = from
@@ -29,8 +29,14 @@ export class StaRH extends Model {
     return merge(
       super.toJSON(),
       {
-        from: this.from,
-        to: this.to,
+        from: {
+          name: this.from.name,
+          avatar: this.from.avatar.toString()
+        },
+        to: {
+          name: this.to.name,
+          avatar: this.to.avatar.toString()
+        },
         amount: this.amount,
         message: this.message
       }
@@ -38,11 +44,22 @@ export class StaRH extends Model {
   }
 
   /**
-   * @param {{from: string, to: string, amount: number, message: string}} data
+   * @param {{from: {name: string, avatar: string}, to: {name: string, avatar: string}, amount: number, message: string}} data
    * @returns {StaRH}
    */
   static fromJSON (data) {
-    return new StaRH(data)
+    return new StaRH({
+      from: {
+        name: data.from.name,
+        avatar: new URIValue(data.from.avatar)
+      },
+      to: {
+        name: data.to.name,
+        avatar: new URIValue(data.to.avatar)
+      },
+      amount: data.amount,
+      message: data.message
+    })
   }
 
   /**
@@ -54,3 +71,4 @@ export class StaRH extends Model {
 }
 
 export const StaRHType = irreducible('StaRHType', (x) => x instanceof StaRH)
+export const PersonType = struct({name: StringType, avatar: URIValue.Type}, 'PersonType')
--- test/starh.spec.js
+++ test/starh.spec.js
@@ -2,29 +2,44 @@
 
 /* global describe, it */
 
-const expect = require('chai').expect
+import {expect} from 'chai'
 import {StaRH, StaRHType} from '../src/starh'
+import URIValue from 'rheactor-value-objects/uri'
 
 describe('StaRH', () => {
   describe('constructor()', () => {
     it('should accept values', () => {
       const staRH = new StaRH({
-        from: 'mtacker',
-        to: 'Antarctica',
+        from: {
+          name: 'Markus Tacker',
+          avatar: new URIValue('https://starhs.net/profileimgs/5d257110-49c4-45e4-b8d5-2b69abf2419d.jpg')
+        },
+        to: {
+          name: 'Heiko Fischer',
+          avatar: new URIValue('https://starhs.net/profileimgs/8651161a-ac33-4837-9c33-87997ce7bdc1.jpg')
+        },
         amount: 1,
         message: 'Test'
       })
       StaRHType(staRH)
-      expect(staRH.from).to.equal('mtacker')
-      expect(staRH.to).to.equal('Antarctica')
+      expect(staRH.from.name).to.equal('Markus Tacker')
+      expect(staRH.from.avatar.equals(new URIValue('https://starhs.net/profileimgs/5d257110-49c4-45e4-b8d5-2b69abf2419d.jpg'))).to.equal(true)
+      expect(staRH.to.name).to.equal('Heiko Fischer')
+      expect(staRH.to.avatar.equals(new URIValue('https://starhs.net/profileimgs/8651161a-ac33-4837-9c33-87997ce7bdc1.jpg'))).to.equal(true)
       expect(staRH.amount).to.equal(1)
       expect(staRH.message).to.equal('Test')
       expect(staRH.$context.equals(StaRH.$context)).to.equal(true)
     })
     it('should parse it\'s own values', () => {
       const staRH = new StaRH({
-        from: 'mtacker',
-        to: 'Antarctica',
+        from: {
+          name: 'Markus Tacker',
+          avatar: new URIValue('https://starhs.net/profileimgs/5d257110-49c4-45e4-b8d5-2b69abf2419d.jpg')
+        },
+        to: {
+          name: 'Heiko Fischer',
+          avatar: new URIValue('https://starhs.net/profileimgs/8651161a-ac33-4837-9c33-87997ce7bdc1.jpg')
+        },
         amount: 1,
         message: 'Test'
       })
@@ -35,8 +50,10 @@ describe('StaRH', () => {
         message: staRH.message
       })
       StaRHType(staRH2)
-      expect(staRH2.from).to.equal('mtacker')
-      expect(staRH2.to).to.equal('Antarctica')
+      expect(staRH2.from.name).to.equal('Markus Tacker')
+      expect(staRH2.to.name).to.equal('Heiko Fischer')
+      expect(staRH2.from.avatar.equals(new URIValue('https://starhs.net/profileimgs/5d257110-49c4-45e4-b8d5-2b69abf2419d.jpg'))).to.equal(true)
+      expect(staRH2.to.avatar.equals(new URIValue('https://starhs.net/profileimgs/8651161a-ac33-4837-9c33-87997ce7bdc1.jpg'))).to.equal(true)
       expect(staRH2.amount).to.equal(1)
       expect(staRH2.message).to.equal('Test')
       expect(staRH2.$context.equals(StaRH.$context)).to.equal(true)
@@ -46,14 +63,22 @@ describe('StaRH', () => {
   describe('JSON', () => {
     it('should parse it\'s JSON representation', () => {
       const staRH = StaRH.fromJSON(JSON.parse(JSON.stringify(new StaRH({
-        from: 'mtacker',
-        to: 'Antarctica',
+        from: {
+          name: 'Markus Tacker',
+          avatar: new URIValue('https://starhs.net/profileimgs/5d257110-49c4-45e4-b8d5-2b69abf2419d.jpg')
+        },
+        to: {
+          name: 'Heiko Fischer',
+          avatar: new URIValue('https://starhs.net/profileimgs/8651161a-ac33-4837-9c33-87997ce7bdc1.jpg')
+        },
         amount: 1,
         message: 'Test'
       }))))
       StaRHType(staRH)
-      expect(staRH.from).to.equal('mtacker')
-      expect(staRH.to).to.equal('Antarctica')
+      expect(staRH.from.name).to.equal('Markus Tacker')
+      expect(staRH.to.name).to.equal('Heiko Fischer')
+      expect(staRH.from.avatar.equals(new URIValue('https://starhs.net/profileimgs/5d257110-49c4-45e4-b8d5-2b69abf2419d.jpg'))).to.equal(true)
+      expect(staRH.to.avatar.equals(new URIValue('https://starhs.net/profileimgs/8651161a-ac33-4837-9c33-87997ce7bdc1.jpg'))).to.equal(true)
       expect(staRH.amount).to.equal(1)
       expect(staRH.message).to.equal('Test')
       expect(staRH.$context.equals(StaRH.$context)).to.equal(true)

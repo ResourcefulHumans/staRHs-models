af3ad66cb3d0f75ebdc966ff707806a2af24b2d4
Markus Tacker <markus@resourceful-humans.com>
2016-12-16 15:50:51 +0100

    test(Model): added.

---
+++ test/model.spec.js
@@ -0,0 +1,65 @@
+'use strict'
+
+/* global describe, it */
+
+import {expect} from 'chai'
+import {Model, ModelType} from '../src/model'
+import URIValue from 'rheactor-value-objects/uri'
+
+const $context = new URIValue('http://example.com/jsonld/some')
+
+describe('Model', () => {
+  describe('constructor()', () => {
+    it('should accept values', () => {
+      const model = new Model({
+        $context: $context,
+        $createdAt: new Date('2016-01-01T00:00:00Z'),
+        $updatedAt: new Date('2016-01-02T00:00:00Z'),
+        $deletedAt: new Date('2016-01-03T00:00:00Z')
+      })
+      ModelType(model)
+      expect(model.$context.equals($context)).to.equal(true)
+      expect(model.$createdAt.toISOString()).to.equal(new Date('2016-01-01T00:00:00Z').toISOString())
+      expect(model.$updatedAt.toISOString()).to.equal(new Date('2016-01-02T00:00:00Z').toISOString())
+      expect(model.$deletedAt.toISOString()).to.equal(new Date('2016-01-03T00:00:00Z').toISOString())
+      expect(model.$links).to.deep.equal([])
+    })
+    it('should parse it\'s own values', () => {
+      const model = new Model({
+        $context: $context,
+        $createdAt: new Date('2016-01-01T00:00:00Z'),
+        $updatedAt: new Date('2016-01-02T00:00:00Z'),
+        $deletedAt: new Date('2016-01-03T00:00:00Z')
+      })
+      const model2 = new Model({
+        $context: model.$context,
+        $createdAt: model.$createdAt,
+        $updatedAt: model.$updatedAt,
+        $deletedAt: model.$deletedAt
+      })
+      ModelType(model2)
+      expect(model2.$context.equals($context)).to.equal(true)
+      expect(model2.$createdAt.toISOString()).to.equal(new Date('2016-01-01T00:00:00Z').toISOString())
+      expect(model2.$updatedAt.toISOString()).to.equal(new Date('2016-01-02T00:00:00Z').toISOString())
+      expect(model2.$deletedAt.toISOString()).to.equal(new Date('2016-01-03T00:00:00Z').toISOString())
+      expect(model2.$links).to.deep.equal([])
+    })
+  })
+
+  describe('JSON', () => {
+    it('should parse it\'s JSON representation', () => {
+      const model = Model.fromJSON(JSON.parse(JSON.stringify(new Model({
+        $context: $context,
+        $createdAt: new Date('2016-01-01T00:00:00Z'),
+        $updatedAt: new Date('2016-01-02T00:00:00Z'),
+        $deletedAt: new Date('2016-01-03T00:00:00Z')
+      }))))
+      ModelType(model)
+      expect(model.$context.equals($context)).to.equal(true)
+      expect(model.$createdAt.toISOString()).to.equal(new Date('2016-01-01T00:00:00Z').toISOString())
+      expect(model.$updatedAt.toISOString()).to.equal(new Date('2016-01-02T00:00:00Z').toISOString())
+      expect(model.$deletedAt.toISOString()).to.equal(new Date('2016-01-03T00:00:00Z').toISOString())
+      expect(model.$links).to.deep.equal([])
+    })
+  })
+})

7273830a3aa183316c5e131e54c63a63ce9ecd54
Markus Tacker <markus@resourceful-humans.com>
2016-12-21 20:26:33 -0500

    fix(Status): parse time into date object, add missing version

--- src/status.js
+++ src/status.js
@@ -1,26 +1,30 @@
-import {String as StringType, Date as DateType, irreducible} from 'tcomb'
+import {String as StringType, Date as DateType, irreducible, refinement, struct} from 'tcomb'
 import URIValue from 'rheactor-value-objects/uri'
+const $context = new URIValue('https://github.com/ResourcefulHumans/staRHs-models#Status')
 
 export class Status {
   /**
-   * @param {{status: string, time: Date}} fields
+   * @param {{status: string, time: Date, version: string}} fields
    */
   constructor (fields) {
-    const {status, time} = fields
+    const {status, time, version} = fields
     StringType(status)
     DateType(time)
+    StringType(version)
     this.status = status
     this.time = time
+    this.version = version
     this.$context = this.constructor.$context
   }
 
   /**
-   * @returns {{status: string, time: string, $context: string}}
+   * @returns {{status: string, time: string, version: string, $context: string}}
    */
   toJSON () {
     return {
       status: this.status,
       time: this.time.toISOString(),
+      version: this.version,
       $context: this.$context.toString()
     }
   }
@@ -30,15 +34,27 @@ export class Status {
    * @returns {Status}
    */
   static fromJSON (data) {
-    return new Status(data)
+    StatusJSONType(data)
+    const {status, time, version} = data
+    return new Status({
+      status,
+      time: new Date(time),
+      version
+    })
   }
 
   /**
    * @returns {URIValue}
    */
   static get $context () {
-    return new URIValue('https://github.com/ResourcefulHumans/staRHs-models#Status')
+    return $context
   }
 }
 
+export const StatusJSONType = struct({
+  $context: refinement(StringType, s => s === $context.toString(), 'StatusContext'),
+  status: StringType,
+  time: StringType,
+  version: StringType
+}, 'StatusJSONType')
 export const StatusType = irreducible('StatusType', (x) => x instanceof Status)

68da9c8b334e4cda44e41862274bfa36180a889a
Markus Tacker <markus@resourceful-humans.com>
2016-12-22 08:38:55 -0500

    fix(Profile): add  to json

--- src/profile.js
+++ src/profile.js
@@ -1,25 +1,29 @@
-import {String as StringType, irreducible, maybe, struct, refinement} from 'tcomb'
+import {String as StringType, irreducible, maybe, struct, refinement, list} from 'tcomb'
 import URIValue from 'rheactor-value-objects/uri'
 import EmailValue from 'rheactor-value-objects/email'
 import {Entity} from './entity'
 import {merge} from 'lodash'
+import {Link, LinkJSONType} from './link'
+const LinkListType = list(Link)
 const $context = new URIValue('https://github.com/ResourcefulHumans/staRHs-models#Profile')
 
 export class Profile extends Entity {
   /**
-   * @param {{$id: string, email: EmailValue, firstname: string, lastname: string, avatar: URIValue}} fields
+   * @param {{$id: string, email: EmailValue, firstname: string, lastname: string, avatar: URIValue, $links: Array.<Link>}} fields
    */
   constructor (fields) {
-    const {$id, email, firstname, lastname, avatar} = fields
+    const {$id, email, firstname, lastname, avatar, $links} = fields
     super({$id, $context})
     StringType(firstname)
     StringType(lastname)
     EmailValue.Type(email)
     maybe(URIValue.Type)(avatar)
+    LinkListType($links || [])
     this.email = email
     this.firstname = firstname
     this.lastname = lastname
     this.avatar = avatar
+    this.$links = $links || []
   }
 
   /**
@@ -48,7 +52,7 @@ export class Profile extends Entity {
   }
 
   /**
-   * @param {{$id: string, email: string, firstname: string, lastname: string, avatar: string}} data
+   * @param {{$id: string, email: string, firstname: string, lastname: string, avatar: string, $links: Array<{href: string, $context: string}>}} data
    * @returns {Profile}
    */
   static fromJSON (data) {
@@ -80,6 +84,7 @@ export const ProfileJSONType = struct({
   email: StringType,
   firstname: StringType,
   lastname: StringType,
-  avatar: StringType
+  avatar: StringType,
+  $links: maybe(list(LinkJSONType))
 }, 'ProfileJSONType')
 export const ProfileType = irreducible('ProfileType', (x) => x instanceof Profile)
